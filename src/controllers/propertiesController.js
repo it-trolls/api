0472aba7ef61fca7c6dce36fd843de94542988ec
ipelaezitec <i.pelaez@itecriocuarto.org.ar>
2019-11-14 01:21:02 -0300

    query string conditions added, still have to refactoring

--- src/controllers/propertiesController.js
+++ src/controllers/propertiesController.js
@@ -24,9 +24,128 @@ exports.propertyDetail = async (req, res) => {
 // GET request for all propertys.
 exports.propertyList = async (req, res) => {
 
-  console.log(req.query);
   try {
-    const propertys = await propertyModel.find(req.query);
+
+    console.log('->', req.query)
+    let query = propertyModel.find({});
+
+    // use limit
+    let limit = parseInt(req.query.limit);
+    if (req.query.limit == undefined){
+      query.limit(10)
+    }else{
+      query.limit(limit)
+    }
+
+    // skip
+    let skip = parseInt(req.query.skip);
+    if (req.query.skip == undefined){
+      query.skip(0)
+    }else{
+      query.skip(skip)
+    }
+
+
+    let search = req.query.search;
+    if (req.query.search != undefined){
+      search.trim()
+      query.find({ "title": { "$regex": search, "$options": "i" } })
+    }
+
+    let description = req.query.description;
+    if (req.query.description != undefined){
+      // Object.keys(req.query.description)
+      query.find({ "description": { "$regex": description, "$options": "i" } })
+    }
+
+    let address = req.query.address;
+    if (req.query.address != undefined){
+      query.find({ "address": { "$regex": address, "$options": "i" } })
+    }
+
+    let neighborhood = req.query.neighborhood;
+    if (req.query.neighborhood != undefined){
+      query.find({ "neighborhood": { "$regex": neighborhood, "$options": "i" } })
+    }
+
+    let floor = req.query.floor;
+    if (req.query.floor != undefined){
+      query.find({ "floor": { "$regex": floor, "$options": "i" } })
+    }
+
+    let type = req.query.type;
+    console.log('what that does mean', type , req.query.type )
+    if (req.query.type != undefined){
+      query.find({"type": type})
+    }
+    
+    let contract = req.query.contract;
+    console.log('contract', contract)
+    if (req.query.contract != undefined){
+      query.find({"contract": contract})
+    }
+
+    let bedrooms = parseInt(req.query.bedrooms);
+    if (req.query.bedrooms != undefined){
+      if (bedrooms > 2) {
+        query.where('bedrooms').gte(2)
+        
+      }else{
+        query.find({"bedrooms": bedrooms})
+      }
+    }
+
+    let bathrooms = parseInt(req.query.bathrooms);
+    if (req.query.bathrooms != undefined){
+      if (bathrooms > 2) {
+        query.where('bathrooms').gte(2)
+        
+      }else{
+        query.find({"bathrooms": bathrooms})
+      }
+    }
+
+    let kitchens = parseInt(req.query.kitchens);
+    if (req.query.kitchens != undefined){
+      if (kitchens > 2) {
+        query.where('kitchens').gte(2)
+        
+      }else{
+        query.find({"kitchens": kitchens})
+      }
+    }
+
+    let antiquity = parseInt(req.query.antiquity);
+    if (req.query.antiquity != undefined){
+      if (antiquity > 2) {
+        query.where('kitchens').gte(2)
+        
+      }else{
+        query.find({"kitchens": antiquity})
+      }
+    }
+
+    let coveredArea = parseInt(req.query.coveredArea);
+    if (req.query.coveredArea != undefined){
+        query.where('coveredArea').gte(coveredArea)
+    }
+
+    // let price = parseInt(req.query.price);
+    // if (req.query.price != undefined){
+    //     query.where('price').gte(price)
+    // }
+
+
+    // let minprice = parseInt(req.query.minprice);
+    // if (req.query.minprice != undefined){
+    //     query.where('price').gte(minprice)
+    // }
+    // query.find({"price": {"$gte": minprice, "$lt": maxprice}})
+
+
+  
+    const propertys = await query.exec();
+    // const propertys = await propertyModel.find(req.query);
     res.status(200).json(propertys);
   } catch (error) {
     res.status(400).send({ 
